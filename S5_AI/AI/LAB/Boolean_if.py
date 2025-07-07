7e5b9a7fc0ad5d8135c6621cdeb15b100c766f1a
Magnel Olivero <magnelolivero@gmail.com>
2025-07-07 11:22:46 +0530

    Create Boolean_if.py

---
+++ S5_AI/AI/LAB/Boolean_if.py
@@ -0,0 +1,10 @@
+is_sunny = True
+have_umbrella = False
+if is_sunny or have_umbrella:
+	print("You should go outside.")
+else:
+	print("You should not go outside.")
+if not is_sunny:
+	print("You should carry the umbrella.")
+else:
+	print("You don't need an umbrella.")
