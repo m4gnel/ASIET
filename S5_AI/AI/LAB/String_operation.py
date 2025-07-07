e98703087c56fdbaabbf1c96c34bd58d49c65d1f
Magnel Olivero <magnelolivero@gmail.com>
2025-07-07 11:23:28 +0530

    Create String_operation.py

---
+++ S5_AI/AI/LAB/String_operation.py
@@ -0,0 +1,12 @@
+s = input(" Enter a sentence : ")
+print(" Number of charecters  : ", len(s))
+print(" Uppercase : ", s.upper())
+print(" Lowercase : ", s.lower())
+print(" Replace spaces with underscores : ", s.replace(" ","_"))
+s.lower()
+if 'python' in s:
+	print(" The word python is in the sentence.")
+else:
+	print(" The word python is not in sentence.")
+ 
+	
